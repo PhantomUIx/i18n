eeebb654bf9f621d0dc8a7058013866e90c751ae
Tristan Ross <tristan.ross@midstall.com>
2023-10-30 23:49:37 -0700

    feat: initial code

---
+++ build.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const no_docs = b.option(bool, "no-docs", "skip installing documentation") orelse false;
+
+    const metaplus = b.dependency("metaplus", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    _ = b.addModule("meta+", .{
+        .source_file = .{
+            .path = metaplus.builder.pathFromRoot(metaplus.module("meta+").source_file.path),
+        },
+    });
+
+    const phantom_i18n = b.addModule("phantom.i18n", .{
+        .source_file = .{ .path = b.pathFromRoot("src/phantom.i18n.zig") },
+        .dependencies = &.{
+            .{
+                .name = "meta+",
+                .module = metaplus.module("meta+"),
+            },
+        },
+    });
+
+    const step_test = b.step("test", "Run all unit tests");
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{
+            .path = b.pathFromRoot("src/phantom.i18n.zig"),
+        },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    unit_tests.addModule("meta+", metaplus.module("meta+"));
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    step_test.dependOn(&run_unit_tests.step);
+
+    const exe_example = b.addExecutable(.{
+        .name = "example",
+        .root_source_file = .{
+            .path = b.pathFromRoot("src/example.zig"),
+        },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe_example.addModule("phantom.i18n", phantom_i18n);
+    b.installArtifact(exe_example);
+
+    if (!no_docs) {
+        const docs = b.addInstallDirectory(.{
+            .source_dir = unit_tests.getEmittedDocs(),
+            .install_dir = .prefix,
+            .install_subdir = "docs",
+        });
+
+        b.getInstallStep().dependOn(&docs.step);
+    }
+}
