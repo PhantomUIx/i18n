e050d40c8b52edd142986c3bdd32638a3c9a9e06
Tristan Ross <tristan.ross@midstall.com>
2023-11-26 12:56:28 -0800

    feat: use new module system

--- src/phantom.zig
+++ src/phantom.zig
@@ -1,5 +1,1 @@
-pub fn import(comptime phantom: type) type {
-    return struct {
-        pub const i18n = @import("phantom/i18n.zig").import(phantom);
-    };
-}
+pub const i18n = @import("phantom/i18n.zig");
--- src/phantom/i18n.zig
+++ src/phantom/i18n.zig
@@ -1,12 +1,6 @@
 const std = @import("std");
 
-pub fn import(comptime _: type) type {
-    return struct {
-        const i18n = @This();
+pub const formats = @import("i18n/formats.zig");
+pub const Format = std.meta.DeclEnum(formats);
 
-        pub const formats = @import("i18n/formats.zig");
-        pub const Format = std.meta.DeclEnum(formats);
-
-        pub usingnamespace @import("i18n/locales.zig");
-    };
-}
+pub usingnamespace @import("i18n/locales.zig");
